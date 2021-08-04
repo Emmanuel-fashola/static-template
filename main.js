7aabf6c63bd82f5398ce560859b037c108b70aa8
Emmanuel-fashola <86474769+Emmanuel-fashola@users.noreply.github.com>
2021-08-04 12:21:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-// getting all required elements
 const inputBox = document.querySelector(".inputField input");
 const addBtn = document.querySelector(".inputField button");
 const todoList = document.querySelector(".todoList");
 const deleteAllBtn = document.querySelector(".footer button");
 
-// onkeyup event
 inputBox.onkeyup = ()=>{
   let userEnteredValue = inputBox.value; //getting user entered value
   if(userEnteredValue.trim() != 0){ //if the user value isn't only spaces
